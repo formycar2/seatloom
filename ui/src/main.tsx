31367a0eee0476f63d1f1ba4409caa5660874420
Xiaolong Zhang <zxlvip@gmail.com>
2026-05-09 14:59:12 +0800

    feat(ui): §B - dynamic header counters and hybrid shell baseline wiring

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/globals.css'
 import './app-v2/styles/tokens.css'
-import App from './App'
+import AppV2 from './app-v2/AppV2'
 import { TopErrorBoundary } from './TopErrorBoundary'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <TopErrorBoundary>
-      <App />
+      <AppV2 />
     </TopErrorBoundary>
   </React.StrictMode>,
 )
